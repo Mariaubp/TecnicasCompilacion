4cda99e7c1c76da7b6fe8941910ffe94d730a05e
Maria Constanza Gigli <76410958+Mariaubp@users.noreply.github.com>
2024-06-23 21:10:02 -0300

    Update CustomErrorListener.java

--- src/main/java/compiladores/CustomErrorListener.java
+++ src/main/java/compiladores/CustomErrorListener.java
@@ -2,10 +2,21 @@ package compiladores;
 
 import org.antlr.v4.runtime.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomErrorListener extends BaseErrorListener {
+    private List<String> syntaxErrors = new ArrayList<>();
+
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,
                             int line, int charPositionInLine, String msg, RecognitionException e) {
-        Escucha.errors.add("Error sintáctico en línea " + line + ":" + charPositionInLine + " - " + msg);
+        String error = "Error sintáctico en línea " + line + ":" + charPositionInLine + " - " + msg;
+        syntaxErrors.add(error);
+        System.err.println(error); // Opcional, para ver errores en la consola
+    }
+
+    public List<String> getSyntaxErrors() {
+        return syntaxErrors;
     }
 }
